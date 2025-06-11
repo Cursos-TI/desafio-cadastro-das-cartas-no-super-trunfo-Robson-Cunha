3d265223bce0000951dc6085e4718e4ea62c2704
Robson-Cunha <robsodin@gmail.com>
2025-06-11 22:55:47 +0000

    Novo Commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,9 @@
 //Teste larissa
 
 int main() {
+    printf("Cartas super Trunfo!\n");
+printf("novo comit\n");
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
